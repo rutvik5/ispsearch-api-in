f9e623435ce6eb7d820f92f3cd37acac578030c3
rutvik5 <rutvik.kolhe97@gmail.com>
2020-05-20 21:23:14 -0400

    created skeleton spring boot app

---
+++ src/main/java/com/ispsearch/ispsearchapiin/IspsearchApiInApplication.java
@@ -0,0 +1,13 @@
+package com.ispsearch.ispsearchapiin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IspsearchApiInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IspsearchApiInApplication.class, args);
+	}
+
+}
